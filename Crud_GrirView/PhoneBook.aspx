c70e8e590274991f006e42e5a844b103f70fe604
sathiyaseelan253 <sathyaseelan253@gmail.com>
2020-02-09 15:56:16 +0530

    Add project files.

---
+++ Crud_GrirView/PhoneBook.aspx
@@ -0,0 +1,103 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PhoneBook.aspx.cs" Inherits="Crud_GrirView.PhoneBook" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        
+        <asp:GridView ID="myPhoneBook" runat="server" AutoGenerateColumns="false" ShowFooter="true" DataKeyNames="Id"  ShowHeaderWhenEmpty="true"
+            onRowCommand="myPhoneBook_RowCommand" onRowEditing="myPhoneBook_RowEditing" OnRowCancelingEdit="myPhoneBook_RowCancelingEdit" OnRowUpdating="myPhoneBook_RowUpdating" OnRowDeleting="myPhoneBook_RowDeleting"
+            
+           
+            
+          
+            CellPadding="4" ForeColor="#333333" GridLines="None" >
+            <AlternatingRowStyle BackColor="White" />
+            <EditRowStyle BackColor="#2461BF" />
+          
+            <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+            <RowStyle BackColor="#EFF3FB" />
+            <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+            <SortedAscendingCellStyle BackColor="#F5F7FB" />
+            <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+            <SortedDescendingCellStyle BackColor="#E9EBEF" />
+            <SortedDescendingHeaderStyle BackColor="#4870BE" />
+
+           <Columns>
+                    <asp:TemplateField HeaderText="First Name">
+                        <ItemTemplate>
+                            <asp:Label Text='<%# Eval("FirstName") %>' runat="server" />
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtFirstName" Text='<%# Eval("FirstName") %>' runat="server" />
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                            <asp:TextBox ID="txtFirstNameFooter" runat="server" />
+                        </FooterTemplate>
+                    </asp:TemplateField>
+                 <asp:TemplateField HeaderText="Last Name">
+                        <ItemTemplate>
+                            <asp:Label Text='<%# Eval("LastName") %>' runat="server" />
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtLastName" Text='<%# Eval("LastName") %>' runat="server" />
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                            <asp:TextBox ID="txtLastNameFooter" runat="server" />
+                        </FooterTemplate>
+                    </asp:TemplateField>
+                 <asp:TemplateField HeaderText="Contact">
+                        <ItemTemplate>
+                            <asp:Label Text='<%# Eval("Contact") %>' runat="server" />
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtContact" Text='<%# Eval("Contact") %>' runat="server" />
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                            <asp:TextBox ID="txtContactFooter" runat="server" />
+                        </FooterTemplate>
+                    </asp:TemplateField>
+                 <asp:TemplateField HeaderText="Email">
+                        <ItemTemplate>
+                            <asp:Label Text='<%# Eval("Email") %>' runat="server" />
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtEmail" Text='<%# Eval("Email") %>' runat="server" />
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                            <asp:TextBox ID="txtEmailFooter" runat="server" />
+                        </FooterTemplate>
+                    </asp:TemplateField>
+               <asp:TemplateField>
+                   <ItemTemplate>
+                       <asp:ImageButton ImageUrl="~/Images/Edit button.png" runat="server" CommandName="Edit" ToolTip="Edit" Width="30px" Height="30px" />
+                        <asp:ImageButton ImageUrl="~/Images/Delete_btn.png" runat="server" CommandName="Delete" ToolTip="Delete" Width="30px" Height="30px" />
+                   </ItemTemplate>
+                   <EditItemTemplate>
+                         <asp:ImageButton ImageUrl="~/Images/Save_btn.png" runat="server" CommandName="Save" ToolTip="Save" Width="30px" Height="30px" />
+                        <asp:ImageButton ImageUrl="~/Images/Cancel_Btn.jpg" runat="server" CommandName="Cancel" ToolTip="Cancel" Width="30px" Height="30px" />
+                   </EditItemTemplate>
+                   <FooterTemplate>
+                       <asp:ImageButton ImageUrl="~/Images/Add_btn.png" runat="server" CommandName="Add" ToolTip="Add" Width="30px" Height="30px" />
+                   </FooterTemplate>
+               </asp:TemplateField>
+               </Columns>
+           
+        </asp:GridView>
+             <br/>
+            <asp:Label ID="lblSuccessMsg" Text="" ForeColor="Green" runat="server"/>
+              <br/>
+            <asp:Label ID="lblErrorMsgFailed" Text="" ForeColor="Red" runat="server"/>
+            </div>
+            </form>
+
+
+</body>
+</html>
---
+++ Crud_GrirView/PhoneBook.aspx.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Crud_GrirView
+{
+    public partial class PhoneBook : System.Web.UI.Page
+    {
+        string connectionString = @"Data Source=LAPTOP-B710EAPP;Integrated Security=true;Initial Catalog=PhoneBookDatabase";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+                PopulateGridView();
+        } 
+        void PopulateGridView()
+        {
+            DataTable table = new DataTable();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("select * from tblPhone", connection);
+                sqlDataAdapter.Fill(table);
+            }
+            if (table.Rows.Count > 1)
+            {
+                myPhoneBook.DataSource = table;
+                myPhoneBook.DataBind();
+            }
+            else
+            {
+                    table.Rows.Add(table.NewRow());
+                    myPhoneBook.DataSource = table;
+                    myPhoneBook.DataBind();
+                    myPhoneBook.Rows[0].Cells.Clear();
+                    myPhoneBook.Rows[0].Cells.Add(new TableCell());
+                    myPhoneBook.Rows[0].Cells[0].ColumnSpan = table.Columns.Count;
+                    myPhoneBook.Rows[0].Cells[0].Text = "No Data Found ..!";
+                    myPhoneBook.Rows[0].Cells[0].HorizontalAlign = HorizontalAlign.Center;
+                }
+            }
+      
+
+       
+
+      
+
+        protected void myPhoneBook_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+                try
+
+                {
+                    if (e.CommandName.Equals("Add"))
+                    {
+                        using (SqlConnection sqlCon = new SqlConnection(connectionString))
+                        {
+                            sqlCon.Open();
+                            string query = "INSERT INTO tblPhone (FirstName,LastName,Contact,Email) VALUES (@FirstName,@LastName,@Contact,@Email)";
+                            SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                            sqlCmd.Parameters.AddWithValue("@FirstName", (myPhoneBook.FooterRow.FindControl("txtFirstNameFooter") as TextBox).Text.Trim());
+                            sqlCmd.Parameters.AddWithValue("@LastName", (myPhoneBook.FooterRow.FindControl("txtLastNameFooter") as TextBox).Text.Trim());
+                            sqlCmd.Parameters.AddWithValue("@Contact", (myPhoneBook.FooterRow.FindControl("txtContactFooter") as TextBox).Text.Trim());
+                            sqlCmd.Parameters.AddWithValue("@Email", (myPhoneBook.FooterRow.FindControl("txtEmailFooter") as TextBox).Text.Trim());
+                            sqlCmd.ExecuteNonQuery();
+                        PopulateGridView();
+                            lblSuccessMsg.Text = "New Record Added";
+                            lblErrorMsgFailed.Text = "";
+                        }
+                    }
+                }
+            catch (Exception ex)
+                {
+                    lblSuccessMsg.Text = "";
+                    lblErrorMsgFailed.Text = ex.Message;
+                }
+            }
+        protected void myPhoneBook_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            myPhoneBook.EditIndex = e.NewEditIndex;
+            PopulateGridView();
+            
+        }
+
+        protected void myPhoneBook_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            myPhoneBook.EditIndex = -1;
+            PopulateGridView();
+        }
+
+        protected void myPhoneBook_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            try
+            {
+                using (SqlConnection sqlCon = new SqlConnection(connectionString))
+                {
+                    sqlCon.Open();
+                    string query = "UPDATE tblPhone SET FirstName=@FirstName,LastName=@LastName,Contact=@Contact,Email=@Email WHERE Id = @id";
+                    SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                    sqlCmd.Parameters.AddWithValue("@FirstName", (myPhoneBook.Rows[e.RowIndex].FindControl("txtFirstName") as TextBox).Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@LastName", (myPhoneBook.Rows[e.RowIndex].FindControl("txtLastName") as TextBox).Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Contact", (myPhoneBook.Rows[e.RowIndex].FindControl("txtContact") as TextBox).Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Email", (myPhoneBook.Rows[e.RowIndex].FindControl("txtEmail") as TextBox).Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@id", Convert.ToInt32(myPhoneBook.DataKeys[e.RowIndex].Value.ToString()));
+                    sqlCmd.ExecuteNonQuery();
+                    myPhoneBook.EditIndex = -1;
+                    PopulateGridView();
+                    lblSuccessMsg.Text = "Selected Record Updated";
+                    lblErrorMsgFailed.Text = "";
+                }
+            }
+            catch (Exception ex)
+            {
+                lblSuccessMsg.Text = "";
+                lblErrorMsgFailed.Text = ex.Message;
+            }
+        }
+
+        protected void myPhoneBook_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            try
+            {
+                using (SqlConnection sqlCon = new SqlConnection(connectionString))
+                {
+                    sqlCon.Open();
+                    string query = "DELETE FROM tblPhone WHERE ID = @id";
+                    SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                    sqlCmd.Parameters.AddWithValue("@id", Convert.ToInt32(myPhoneBook.DataKeys[e.RowIndex].Value.ToString()));
+                    sqlCmd.ExecuteNonQuery();
+                    PopulateGridView();
+                    lblSuccessMsg.Text = "Selected Record Deleted";
+                    lblErrorMsgFailed.Text = "";
+                }
+            }
+            catch (Exception ex)
+            {
+                lblSuccessMsg.Text = "";
+                lblErrorMsgFailed.Text = ex.Message;
+            }
+        }
+    }
+}
